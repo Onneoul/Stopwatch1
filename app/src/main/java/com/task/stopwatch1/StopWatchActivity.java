d55c496649a9b895822036d94d20cf2191cff95d
Onneoul <kimminse99@naver.com>
2022-11-10 15:00:30 +0900

    Second Version Rotate Support

--- app/src/main/java/com/task/stopwatch1/StopWatchActivity.java
+++ app/src/main/java/com/task/stopwatch1/StopWatchActivity.java
@@ -13,12 +13,43 @@ public class StopWatchActivity extends AppCompatActivity {
 
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        // 앱의 화면이 처음 구성될 때 savedInstanceState는 null
+        // 화면이 다시 생성되고 파괴되기 전에 onSaveInstanceState가 호출되면 생성된 Bundle 객체를 액티비티에 전달
+        // 해당 메소드로 화면이 회전했을 때도 데이터를 유지할 수 있음
         runTimmer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+        //메소드는 Bundle 객체를 인자로 받음, Bundle 객체는 여러 타입의 데이터를 하나의 객체로 만듦
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    protected void onResume() {
+        super.onResume();
+        if(wasRunning) {
+            running = true;
+        }
     }
 
     public void onClickStart(View view) {
@@ -33,7 +64,7 @@ public class StopWatchActivity extends AppCompatActivity {
     }
 
     private void runTimmer() {
-        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
@@ -46,7 +77,7 @@ public class StopWatchActivity extends AppCompatActivity {
                 if(running) {
                     seconds++;
                 }
-                handler.postDelayed(this, 1000);
+                handler.postDelayed(this, 1000); // 1000 Millisecond마다 this 반복
             }
         });
     }
